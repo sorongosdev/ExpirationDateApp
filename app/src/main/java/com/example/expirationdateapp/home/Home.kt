f4cf48bdae88c5f07230176568151b6239ea31c6
dosora1 <dosora9@gmail.com>
2022-11-27 21:41:52 +0900

    Listview, LiveData observe 구현
    -수신 객체 자신을 반환하는 apply로 리스트뷰 가져오기
    -아이템리스트를 라이브데이터로, 옵저버로 뷰모델에서 가져오기

--- app/src/main/java/com/example/expirationdateapp/home/Home.kt
+++ app/src/main/java/com/example/expirationdateapp/home/Home.kt
@@ -8,32 +8,36 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.expirationdateapp.R
 import com.example.expirationdateapp.databinding.FragmentHomeBinding
-import com.google.firebase.firestore.*
-import org.w3c.dom.Document
 
 class Home : Fragment() {
     private lateinit var binding: FragmentHomeBinding
-    val db = FirebaseFirestore.getInstance()
-    val itemList = arrayListOf<ListLayout>()
-    val adapter = ListAdapter(itemList)
+    lateinit var model: MainViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home,container,false)
-        binding.lifecycleOwner =viewLifecycleOwner
 
         //recycer view and adapter
-        binding.rvList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
-        binding.rvList.adapter = adapter
+        binding.rvList.apply{
+            layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+            adapter =
+                ListAdapter(
+                    layoutInflater,
+                    emptyList()
+                )
+        }
+
+        //viewmodel
+        model = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+
+        model.LiveItemListData.observe(viewLifecycleOwner){
+            (binding.rvList.adapter as ListAdapter).setData(it)
+        }
 
         //아이템 추가하는 화면으로 전환
         binding.btnAdd.setOnClickListener{
@@ -41,23 +45,11 @@ class Home : Fragment() {
             //화면 종료
         }
 
-        //Firestore Read, collection이름으로 field의 name 요소 불러오기
-        db.collection("player").get()   // 작업할 컬렉션에서 문서 가져오기
-            .addOnSuccessListener { result ->
-                //firestore write
-                for (doc in result) {  // 가져온 문서들은 result에 들어감
-                    val item = ListLayout(doc["name"] as String, doc["useby"] as String)
-                    itemList.add(item)
-                }
-                adapter.notifyDataSetChanged()
-            }
-
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         Log.d(TAG, "onDestroyView")
-        itemList.clear()
     }
 }
