4f734c24ccf3a8c618c46a52197c277188a55f74
dosora1 <dosora9@gmail.com>
2022-11-27 21:44:14 +0900

    Update NaviActivity.kt

--- app/src/main/java/com/example/expirationdateapp/home/NaviActivity.kt
+++ app/src/main/java/com/example/expirationdateapp/home/NaviActivity.kt
@@ -1,8 +1,8 @@
 package com.example.expirationdateapp.home
 
-import android.arch.lifecycle.ViewModelProvider
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -11,9 +11,9 @@ import com.example.expirationdateapp.databinding.ActivityNaviBinding
 
 class NaviActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNaviBinding
-    private lateinit var viewModel : MainViewModel
 
-//    private val TAG = NaviActivity::class.java.simpleName
+//    private val viewModel : MainViewModel by viewModels<MainViewModel>()
+////    private val TAG = NaviActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,7 +23,9 @@ class NaviActivity : AppCompatActivity() {
         // fragment controller
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.findNavController()
-
         binding.navigationView.setupWithNavController(navController)
+
+//        // viewmodel
+//        viewModel.reload()
     }
 }
